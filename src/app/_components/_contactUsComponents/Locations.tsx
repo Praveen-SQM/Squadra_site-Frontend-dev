d7724b20b9863d1c3fd94ec48a8021acc6a23ba3
Kuldeep Singh Jadon <kuldeep@squadramedia.com>
2025-01-10 19:44:04 +0530

    Modification in Location Images

--- src/app/_components/_contactUsComponents/Locations.tsx
+++ src/app/_components/_contactUsComponents/Locations.tsx
@@ -32,7 +32,7 @@ export default function Locations() {
             </div>
           </div>
 
-          <div className="flex flex-col lg:gap-[32px] md:gap-[16px] sm:gap-4 gap-4">
+          {/* <div className="flex flex-col lg:gap-[32px] md:gap-[16px] sm:gap-4 gap-4">
             <div className="xl:w-[380px] xl:h-[380px] lg:w-[300px] lg:h-[300px] md:h-[230px] md:w-[230px] sm:h-[230px] sm:w-[230px]">
               <Image
                 src={location2}
@@ -62,7 +62,7 @@ export default function Locations() {
               <p className="font-medium lg:text-[24.9px] md:text-[18px] leading-[27.38px] text-[#131313]">Bangalore, India</p>
               <p className="font-normal lg:text-[16px] lg:leading-6 md:text-[14px] md:leading-[19px] text-[#6D6D6D] lg:max-w-[380px] md:max-w-[230px]">5th Floor, No. 1134, Shreeram Niwas, 100 Feet Rd, HAL 2nd Stage, Bengaluru, Karnataka 560038</p>
             </div>
-          </div>
+          </div> */}
         </div>
 
         <div className="flex lg:gap-[4px] sm:gap-[4px] gap-[2px] items-center lg:mt-[50px] md:mt-[40px] mt-[32px]">
@@ -92,7 +92,7 @@ export default function Locations() {
                 />
                 <p className="font-normal lg:text-[20px] lg:leading-[26px] sm:text-[20px] sm:leading-[26px] text-[14px] leading-[16.94px] text-[#6D6D6D]">HR Enquiry</p>
               </div>
-              <p className="font-semibold lg:text-[32px] lg:leading-[38.73px] md:text-[28px] md:leading-[33.89px] text-[16px] leading-[19.36px] text-[#3D3D3D]">+91 96068 27067</p>
+              <p className="font-semibold lg:text-[32px] lg:leading-[38.73px] md:text-[28px] md:leading-[33.89px] text-[16px] leading-[19.36px] text-[#3D3D3D]">hr@squadramedia.com</p>
             </div>
           </div>
         </div>
