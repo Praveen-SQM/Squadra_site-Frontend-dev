0fb58d59beef60c32790226d6b757b6a3e7d25d3
Kuldeep Singh Jadon <kuldeep@squadramedia.com>
2025-01-10 21:52:20 +0530

    changed years of experience

--- src/app/_components/_landingPageComponents/PeoplesWoGetsThingsDone.tsx
+++ src/app/_components/_landingPageComponents/PeoplesWoGetsThingsDone.tsx
@@ -118,7 +118,7 @@ function PeoplesWoGetsThingsDone() {
     <div>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-0 flex items-center justify-between mb-8 mt-4 md:mt-0 md:mb-12">
         <h1 className="text-3xl sm:text-4xl md:text-4xl lg:text-4xl font-[600] lg:max-w-[570px]">
-          Empowering <strong className="text-[#FBAE17] font-[600]">Brands</strong> with Media, Tech, and Animation.
+          Empowering <strong className="text-[#FBAE17] font-[600]">Brands</strong> with Media Tech and Animation.
         </h1>
         <div className="hidden lg:block">
           <div className="h-[56px] w-[173px] rounded-[42px] border py-4 px-5 gap-[4px] flex justify-center items-center bg-[#FFFFFF] border-[#E7E7E7] cursor-pointer">
@@ -183,7 +183,7 @@ function PeoplesWoGetsThingsDone() {
             <div className="hidden md:block">
               <div className="flex items-center justify-center gap-6">
                 <div className="w-[221px] h-[130px] flex flex-col justify-center items-center rounded-[24px] border border-[#E7E7E7] bg-[#F8F8F8] p-6">
-                  <p ref={ref} className="font-medium text-[52px] leading-[62.05px] text-[#FBAE17]">{inView ? <CountUp end={4} duration={3} /> : 40}+</p>
+                  <p ref={ref} className="font-medium text-[52px] leading-[62.05px] text-[#FBAE17]">{inView ? <CountUp end={4} duration={3} /> : 4}+</p>
                   <p className="font-normal text-[18px] leading-[32px] text-[#6D6D6D]">Years of Experience</p>
                 </div>
                 <div className="w-[221px] h-[130px] flex flex-col justify-center items-center rounded-[24px] border border-[#E7E7E7] bg-[#F8F8F8] p-6">
@@ -201,7 +201,7 @@ function PeoplesWoGetsThingsDone() {
               <div className="flex flex-col gap-3">
               <div className="flex items-center justify-center gap-3">
                 <div className="w-[162px] h-[64px] flex flex-col justify-center items-center rounded-[11px] border border-[#E7E7E7] bg-[#F8F8F8] p-[11.26px]">
-                  <p ref={ref} className="font-medium text-[24px] leading-[28.64px] text-[#FBAE17]">{inView ? <CountUp end={4} duration={3} /> : 40}+</p>
+                  <p ref={ref} className="font-medium text-[24px] leading-[28.64px] text-[#FBAE17]">{inView ? <CountUp end={4} duration={3} /> : 4}+</p>
                   <p className="font-normal text-[13px] leading-[15px] text-[#6D6D6D]">Years of Experience</p>
                 </div>
                 <div className="w-[162px] h-[64px] flex flex-col justify-center items-center rounded-[11px] border border-[#E7E7E7] bg-[#F8F8F8] p-[11.26px]">
