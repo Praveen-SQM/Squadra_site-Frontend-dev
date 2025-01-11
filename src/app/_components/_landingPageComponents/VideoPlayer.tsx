c5d74d4e75d6292b3d01d71d3aa612cf0c5441d0
Kuldeep Singh Jadon <kuldeep@squadramedia.com>
2025-01-11 20:13:30 +0530

    removed unused imports

--- src/app/_components/_landingPageComponents/VideoPlayer.tsx
+++ src/app/_components/_landingPageComponents/VideoPlayer.tsx
@@ -1,7 +1,4 @@
 "use client";
-import dynamic from 'next/dynamic';
-
-const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
 
 export default function VideoPlayer() {
     return (
