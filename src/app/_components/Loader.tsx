48c3d01053663cac517eadeaf5f09e0ba6734dd6
Saurabhsanjay <saurabhubale371@gmail.com>
2025-01-12 15:36:26 +0530

    videeo updated

--- src/app/_components/Loader.tsx
+++ src/app/_components/Loader.tsx
@@ -5,12 +5,15 @@ const InitialVideoLoader: React.FC = () => {
   const [isVideoPlayed, setIsVideoPlayed] = useState(false);
 
   useEffect(() => {
-    const videoPlayed = sessionStorage.getItem('videoPlayed');
-    if (!videoPlayed) {
-      setIsVideoPlayed(true);
-      sessionStorage.setItem('videoPlayed', 'true');
-    }
+
+    setIsVideoPlayed(true);
+
+
   }, []);
+
+  const handleVideoEnd = () => {
+    setIsVideoPlayed(false);
+  };
 
   if (!isVideoPlayed) {
     return null;
@@ -22,10 +25,9 @@ const InitialVideoLoader: React.FC = () => {
         autoPlay
         muted
         playsInline
-        width="50%"
-        height="50%"
-        onEnded={() => setIsVideoPlayed(false)} 
-        className="video"
+        className="w-full sm:w-3/4 md:w-1/2 lg:w-1/3"
+        preload="auto"
+        onEnded={handleVideoEnd}
       >
         <source
           src="https://squadra-media.s3.ap-south-1.amazonaws.com/Saurabh+Ubale%27s+Video+-+Jan+12%2C+2025-VEED+%28online-video-cutter.com%29.mp4"
