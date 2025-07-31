3bdbdf38f88d78a05bb19c3269d45bb91f677f74
Saurabhsanjay <saurabhubale371@gmail.com>
2025-07-31 12:41:04 +0530

    refactor: remove unused variable declaration in GET function

--- src/app/api/jobs/[id]/route.ts
+++ src/app/api/jobs/[id]/route.ts
@@ -8,7 +8,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
