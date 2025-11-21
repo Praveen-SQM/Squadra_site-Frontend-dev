30c3a420c1032f0fd7227ff7034704944bb125bd
Kuldeep Singh Jadon <kuldeep@squadramedia.com>
2025-11-21 21:19:54 +0530

    made gtm tag only for squadraverse page

--- src/app/animation-and-e-learning/page.tsx
+++ src/app/animation-and-e-learning/page.tsx
@@ -1,18 +1,6 @@
-// import React from 'react'
-// import ELearning from '../../app/_components/AnimationandElearningComponents/Index'
+"use client";
 
-// const page = () => {
-//   return (
-//     <ELearning/>
-//   )
-// }
-
-// export default page
-
-
-
-
-
+import { useEffect } from "react";
 import Hero from "../_components/SquadraVerseComponents/Hero";
 import Capabilities from "../_components/SquadraVerseComponents/Capabilities";
 import Industries from "../_components/SquadraVerseComponents/Industries";
@@ -23,11 +11,50 @@ import Ecosystem from "../_components/SquadraVerseComponents/Ecosystem";
 import VideoShowcase from "../_components/SquadraVerseComponents/Videoshowcase";
 import FloatingActionButton from "../_components/SquadraVerseComponents/Floatingactionbutton";
 
-export default function page() {
+export default function Page() {
+  useEffect(() => {
+    // Add GTM script
+    const script = document.createElement("script");
+    script.innerHTML = `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-K7RW3ZDV');
+    `;
+    document.head.appendChild(script);
+
+    // Cleanup function - runs when navigating away
+    return () => {
+      // Remove the inline script
+      document.head.removeChild(script);
+
+      // Remove GTM-related scripts
+      const gtmScripts = document.querySelectorAll(
+        'script[src*="googletagmanager.com"]'
+      );
+      gtmScripts.forEach((s) => s.remove());
+
+      // Clear dataLayer
+      if (window.dataLayer) {
+        window.dataLayer = [];
+      }
+    };
+  }, []);
+
   return (
     <main>
-      {/* <Navbar /> */}
-      <Hero/>
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-K7RW3ZDV"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
+
+      <Hero />
       <Capabilities />
       <Industries />
       <WhyUs />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import TopLoader from "./_components/Toploader";
 import ClientLoaderWrapper from "./_components/ClientLoaderWrapper";
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Squadra Media",
@@ -18,33 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        {/* Google Tag Manager */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K7RW3ZDV');`,
-          }}
-        />
-        {/* End Google Tag Manager */}
-      </head>
+      <head></head>
       <body className="antialiased">
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K7RW3ZDV"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-
         <TopLoader />
         <Toaster />
         <ClientLoaderWrapper>{children}</ClientLoaderWrapper>
